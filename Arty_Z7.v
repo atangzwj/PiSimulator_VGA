afad52afbe997abf5fcc9779d185704791401b07
Arvin Tang <atangzwj@outlook.com>
2018-05-26 19:26:54 -0700

    Removed unneeded Pmod pins and added reset button and muxes

--- Arty_Z7.v
+++ Arty_Z7.v
@@ -1,8 +1,9 @@
 module Arty_Z7 (
    output wire [4:1] ja_p,
    output wire [4:1] ja_n,
-   output wire [4:1] jb_p,
-   output wire [4:1] jb_n,
+   output wire [3:1] jb_p,
+   output wire [3:1] jb_n,
+   input  wire [0:0] btn,
    input  wire       clk
 );
 
@@ -10,6 +11,7 @@ module Arty_Z7 (
    wire [3:0] g;
    wire [3:0] b;
    wire       HS, VS;
+
    assign ja_p[1] = r[0];
    assign ja_n[1] = r[1];
    assign ja_p[2] = r[2];
@@ -25,6 +27,64 @@ module Arty_Z7 (
    assign jb_n[2] = b[3];
    assign jb_p[3] = HS;
    assign jb_n[3] = VS;
-   assign jb_p[4] = 1'b0;
-   assign jb_n[4] = 1'b0;
+
+   wire vidSel;
+   PiSimulator_VGA pisim_vga (
+      .HS(HS),
+      .VS(VS),
+      .vidSel(vidSel),
+      .clk100(clk),
+      .reset(btn[0])
+   );
+
+   busMux2_1 #(.WIDTH(4)) r_mux (
+      .out(r),
+      .in0(4'hC),
+      .in1(4'hF),
+      .sel(vidSel)
+   );
+   busMux2_1 #(.WIDTH(4)) g_mux (
+      .out(g),
+      .in0(4'hA),
+      .in1(4'h0),
+      .sel(vidSel)
+   );
+   busMux2_1 #(.WIDTH(4)) b_mux (
+      .out(b),
+      .in0(4'h7),
+      .in1(4'h0),
+      .sel(vidSel)
+   );
+endmodule
+
+module busMux2_1 #(parameter WIDTH = 64) (
+   output wire [WIDTH - 1 : 0] out,
+   input  wire [WIDTH - 1 : 0] in0,
+   input  wire [WIDTH - 1 : 0] in1,
+   input  wire                 sel
+);
+
+   genvar i;
+   generate
+      for (i = 0; i < WIDTH; i = i + 1) begin : muxes
+         mux2_1 m (.out(out[i]), .in0(in0[i]), .in1(in1[i]), .sel);
+      end
+   endgenerate
+endmodule
+
+module mux2_1 (
+   output wire out,
+   input  wire in0,
+   input  wire in1,
+   input  wire sel
+);
+
+   wire seln;
+   not n (seln, sel);
+
+   wire out0, out1;
+   and a0 (out0, in0, seln);
+   and a1 (out1, in1, sel);
+
+   or o (out, out0, out1);
 endmodule
